fe589eb064a3f83d9f2068d57be8d547c1f3b1d1
RusWerty <ibeneyev.ruslan@gmail.com>
2024-02-28 15:48:43 +0600

    Изменил локатор

--- lesson14_step13.py
+++ lesson14_step13.py
@@ -1,42 +1,46 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-import unittest
 
 class TestAbs(unittest.TestCase):
-	def test_abs1(self):
-		link = "http://suninjuly.github.io/registration1.html"
-		browser = webdriver.Chrome()
-		browser.get(link)
-		input1 = browser.find_element(By.XPATH,"//input[@class='form-control first']")
-		input1.send_keys("Smolensk")
-		input2 = browser.find_element(By.XPATH,"//input[@class='form-control second']")
-		input2.send_keys("Smoll")
-		input3 = browser.find_element(By.XPATH,"//input[@class='form-control third' and @placeholder='Input your email']")
-		input3.send_keys("Eensk@mail.kz")
-		button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-		button.click()
-		time.sleep(10)
-		welcome_text_elt = browser.find_element(By.XPATH, "//h1")
-		welcome_text = welcome_text_elt.text
-		self.assertEqual ("Congratulations! You have successfully registered!" == welcome_text, "first test failed")
+    def test_rts1(self):
+        link = " http://suninjuly.github.io/registration1.html"
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(10)
+        self.browser.get(link)
+        input1 = self.browser.find_element(By.CSS_SELECTOR, '.first_block .first')
+        input1.send_keys("Smolensk")
+        input3 = self.browser.find_element(By.CSS_SELECTOR, '.first_block .third')
+        input3.send_keys("Eensk@mail.kz")
+        input2 = self.browser.find_element(By.CSS_SELECTOR, '.first_block .second')
+        input2.send_keys("Smoll")
+        button = self.browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+        welcome_text_elt = self.browser.find_element(By.XPATH, "//h1")
+        welcome_text = welcome_text_elt.text
+        self.assertEqual ("Congratulations! You have successfully registered!", welcome_text)
+        time.sleep(1)
+        self.browser.quit()
 
-	def test_abs1(self):
-		link = "http://suninjuly.github.io/registration2.html"
-		browser = webdriver.Chrome()
-		browser.get(link)
-		input1 = browser.find_element(By.XPATH,"//input[@class='form-control first']")
-		input1.send_keys("Smolensk")
-		input2 = browser.find_element(By.XPATH,"//input[@class='form-control second']")
-		input2.send_keys("Smoll")
-		input3 = browser.find_element(By.XPATH,"//input[@class='form-control third' and @placeholder='Input your email']")
-		input3.send_keys("Eensk@mail.kz")
-		button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-		button.click()
-		time.sleep(10)
-		welcome_text_elt = browser.find_element(By.XPATH, "//h1")
-		welcome_text = welcome_text_elt.text
-		self.assertEqual ("Congratulations! You have successfully registered!" == welcome_text, "second test failed")
+    def test_rts2(self):
+        link = " http://suninjuly.github.io/registration2.html"
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(10)
+        self.browser.get(link)
+        input1 = self.browser.find_element(By.CSS_SELECTOR, '.first_block .first')
+        input1.send_keys("Smolensk")
+        input3 = self.browser.find_element(By.CSS_SELECTOR, '.first_block .third')
+        input3.send_keys("Eensk@mail.kz")
+        input2 = self.browser.find_element(By.CSS_SELECTOR, '.first_block .second')
+        input2.send_keys("Smoll")
+        button = self.browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+        welcome_text_elt = self.browser.find_element(By.XPATH, "//h1")
+        welcome_text = welcome_text_elt.text
+        self.assertEqual ("Congratulations! You have successfully registered!", welcome_text)
+        time.sleep(1)
+        self.browser.quit()
 
 if __name__ == "__main__":
     unittest.main()
